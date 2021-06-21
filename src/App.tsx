bc4d8a79fdd34e1610fdd34a2196f45c1d7b6e50
kuangshp <kuangshp@126.com>
2021-06-21 11:31:26 +0800

    feat: 配置路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import styles from './App.module.scss';
-import { Home } from './views';
+import { Home, Personal } from './views';
 import { ErrorBoundary, FullPageErrorFallback } from 'components';
-
+import { HashRouter, Link } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router';
 function App(): React.ReactElement {
   return (
     <div className={styles.container}>
       <ErrorBoundary fallbackRender={FullPageErrorFallback}>
-        <Home />
+        <HashRouter>
+          <ul>
+            <li>
+              <Link to="/">到首页</Link>
+            </li>
+            <li>
+              <Link to="/personal">到个人中心</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/personal" element={<Personal />} />
+            <Navigate to="/" />
+          </Routes>
+        </HashRouter>
       </ErrorBoundary>
     </div>
   );
---
+++ src/views/personal/Personal.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Personal: React.FC = () => {
+  return (
+    <>
+      <h2>个人中心</h2>
+    </>
+  );
+};
