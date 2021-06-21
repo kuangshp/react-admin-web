3aa1cf0f94a2f19e5bfe720d00e85f89b2e19e41
kuangshp <kuangshp@126.com>
2021-06-21 09:20:12 +0800

    feat: 配置样式提示

---
+++ src/types/custom.d.ts
@@ -0,0 +1,9 @@
+declare module '*.css' {
+  const css: { [key: string]: string };
+  export default css;
+}
+
+declare module '*.scss' {
+  const scss: { [key: string]: string };
+  export default scss;
+}
