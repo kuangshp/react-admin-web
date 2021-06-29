880930bfe2b6e39f8e08511c803b5a93b988817a
kuangshp <kuangshp@126.com>
2021-06-29 11:46:54 +0800

    feat: 修改app.tsx组件

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -1,14 +1,7 @@
-// import React, { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 
-// type Props = { children: ReactNode };
-// export const App: React.FC<Props> = (props: Props): JSX.Element => {
-//   const { children } = props;
-//   return <div>{children}</div>;
-// };
-
-import React from 'react';
-
-export const App: React.FC = (props) => {
+type Props = { children: ReactNode };
+export const App: React.FC<Props> = (props: Props): JSX.Element => {
   const { children } = props;
   return <div>{children}</div>;
 };
