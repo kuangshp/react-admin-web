e86fcb83b6641afbc7b1bbfd4d7967678c245f74
kuangshp <kuangshp@126.com>
2021-06-30 16:13:16 +0800

    feat: 修复路由拦截

--- src/router/PrivateRoute/PrivateRoute.tsx
+++ src/router/PrivateRoute/PrivateRoute.tsx
@@ -18,27 +18,25 @@ export const PrivateRoute = (props: Props): React.ReactElement | null => {
   // 这里使用自定义的方式获取token,因为前端自定义了token失效时间
   const token: string | null = storage.getItem(authToken);
   const menusList = useSelector((state: RootState) => state.menus.menusList);
-  console.log('进来了', token, path);
   if (!Component) return null;
   if (!token) {
     return <Navigate to="/login" />;
   }
   // 判断当前路径是否在后端返回的接口菜单中
-  console.log(menusList, url);
   const currentRoute = menusList.find((item: IMenusVo) => item.url === url);
-  console.log(currentRoute, '??');
   if (currentRoute) {
-    console.log('11111');
     return (
       <DocumentTitle title={title}>
         <Route path={path} element={<Component />} />
       </DocumentTitle>
     );
-  } else {
+  } else if (path === '/home') {
     return (
       <DocumentTitle title="首页">
-        <Route path={path} element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </DocumentTitle>
     );
+  } else {
+    return <Navigate to="/home" replace={true} />;
   }
 };
