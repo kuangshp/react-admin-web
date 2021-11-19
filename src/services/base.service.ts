80ed13164523123f4c9561a1910a872a12ce715f
kuangshp <kuangshp@126.com>
2021-11-19 21:12:33 +0800

    feat: 添加接口请求缓存

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -1,21 +1,49 @@
 import axios from 'axios';
+import { Cacheables } from 'cacheables';
+import { MAX_AGE } from 'src/constants';
 import { ObjectType, objCleanEmpty, objValueTrim } from '../utils';
+
+const cache = new Cacheables({
+  logTiming: false,
+  log: false,
+});
 
 export default class BaseService {
   // 提供get请求方法
-  static async get<T>(url: string, params: ObjectType = {}, clearEmptyParams = true): Promise<T> {
+  static async get<T>(
+    url: string,
+    params: ObjectType = {},
+    clearEmptyParams = true,
+    isCache = true
+  ): Promise<T> {
     params = clearEmptyParams ? objCleanEmpty(objValueTrim(params)) : params;
-    return await axios.request({
-      method: 'GET',
-      url,
-      params,
-    });
+    return cache.cacheable(
+      () =>
+        axios.request({
+          method: 'GET',
+          url,
+          params,
+        }),
+      BaseService.cacheKey('GET', url),
+      {
+        cachePolicy: 'max-age',
+        maxAge: isCache ? MAX_AGE : 0,
+      }
+    );
   }
 
   // 提供post请求
-  static async post<T>(url: string, postData: ObjectType, clearEmptyData = true): Promise<T> {
+  static async post<T>(
+    url: string,
+    postData: ObjectType,
+    clearEmptyData = true,
+    isCache = true
+  ): Promise<T> {
     postData = clearEmptyData ? objCleanEmpty(objValueTrim(postData)) : objValueTrim(postData);
-    return await axios.post(url, postData);
+    return cache.cacheable(() => axios.post(url, postData), BaseService.cacheKey('POST', url), {
+      cachePolicy: 'max-age',
+      maxAge: isCache ? MAX_AGE : 0,
+    });
   }
 
   // 提供patch请求
@@ -23,10 +51,18 @@ export default class BaseService {
     url: string,
     id: number | string,
     postData: ObjectType = {},
-    clearEmptyData = true
+    clearEmptyData = true,
+    isCache = true
   ): Promise<T> {
     postData = clearEmptyData ? objCleanEmpty(objValueTrim(postData)) : objValueTrim(postData);
-    return await axios.patch(`${url}/${id}`, objValueTrim(postData ?? {}));
+    return cache.cacheable(
+      () => axios.patch(`${url}/${id}`, objValueTrim(postData ?? {})),
+      BaseService.cacheKey('PATCH', `${url}/${id}`),
+      {
+        cachePolicy: 'max-age',
+        maxAge: isCache ? MAX_AGE : 0,
+      }
+    );
   }
 
   // 提供put方法
@@ -34,14 +70,33 @@ export default class BaseService {
     url: string,
     id: number | string,
     postData: ObjectType = {},
-    clearEmptyData = true
+    clearEmptyData = true,
+    isCache = true
   ): Promise<T> {
     postData = clearEmptyData ? objCleanEmpty(objValueTrim(postData)) : objValueTrim(postData);
-    return await axios.put(`${url}/${id}`, objValueTrim(postData ?? {}));
+    return cache.cacheable(
+      () => axios.put(`${url}/${id}`, objValueTrim(postData)),
+      BaseService.cacheKey('PUT', `${url}/${id}`),
+      {
+        cachePolicy: 'max-age',
+        maxAge: isCache ? MAX_AGE : 0,
+      }
+    );
   }
 
   // 提供delete请求
-  static async delete<T>(url: string, id: number | string): Promise<T> {
-    return await axios.delete(`${url}/${id}`);
+  static async delete<T>(url: string, id: number | string, isCache = true): Promise<T> {
+    return cache.cacheable(
+      () => axios.delete(`${url}/${id}`),
+      BaseService.cacheKey('DELETE', `${url}/${id}`),
+      {
+        cachePolicy: 'max-age',
+        maxAge: isCache ? MAX_AGE : 0,
+      }
+    );
+  }
+
+  private static cacheKey(method: string, url: string): string {
+    return `${method}_${url}`;
   }
 }
